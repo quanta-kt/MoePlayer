e84225cbfbbad77138e19a5712f0f9d3a14fa560
Sonar Abhijeet <abhijeet.nkt@gmail.com>
2023-04-04 11:43:36 +0530

    Migrate to version catalog and kotlin build script

---
+++ app/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
+}
+
+android {
+    namespace = "com.github.quantakt.moeplayer"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.github.quantakt.moeplayer"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.0"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.bundles.compose)
+
+    implementation(libs.hilt)
+    kapt(libs.hilt.compiler)
+
+    implementation(libs.bundles.ktor.client)
+
+    implementation(libs.kotlinx.serialization.json)
+
+    implementation(libs.junit)
+    implementation(libs.androidx.junit.ext)
+    implementation(libs.espresso)
+}
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
+}
